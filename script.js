d2f79553b54ce05b075ac5bc8d092332a87964fb
daniil54sharov <daniilsharov54@gmail.com>
2025-11-30 12:57:28 +0200

    showing buildings instead of districts

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([50.45, 30.52], 12);
+var map = L.map('map', { preferCanvas: true }).setView([50.45, 30.52], 12);
 
 L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
   maxZoom: 20,
@@ -6,22 +6,17 @@ L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{
 }).addTo(map);
 
 function getColor(s) {
-    return s == 3 ? '#808080' :
-           s == 2  ? '#800000ff' :
-                      '#008000ff' ;
+    return s == 3 ? '#808080ff' :
+           s == 2 ? '#800000ff' :
+                    '#008000ff' ;
 }
 
 function style(feature) {
     return {
-        fillColor: getColor(feature.properties.status),
-        weight: 3,
-        opacity: 1,
-        color: '#004000ff',
-        dashArray: '1',
+        stroke: false,
+        fillColor: '#00a500ff',
         fillOpacity: 0.7
     };
 }
 
-
-L.geoJson(kyiv_districts).addTo(map);
 L.geoJson(kyiv_districts, {style: style}).addTo(map)
